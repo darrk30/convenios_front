90d490ba5fa877639e155f609b375532d47f2cba
Cesar Enrique Gago Egocheaga <especialistaoti41@itp.gob.pe>
2025-06-02 11:22:58 -0500

    limpiando rama principal

--- src/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production : false,
-  // Producción
-  // apiUrl: 'https://srvwinbe.itp.gob.pe/acuerdosapi',
-
-  // Desarrollo
-  apiUrl: 'https://srvwinbedev.itp.gob.pe/acuerdosapi/api',
-
-};
